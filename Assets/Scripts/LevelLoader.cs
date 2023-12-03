6a2ad5ab51ebba757ad8e17759980c55773b34ea
Valeriy <dobrotskiyvd@gmail.com>
2023-12-04 00:31:02 +0200

    Started adding main menu, added LevelLoader for handling loading between levels.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    private static string SceneToLoad { set; get; } = "MainMenu";
+
+    [SerializeField] private Slider _slider;
+    [SerializeField] private TextMeshProUGUI _textValueField;
+
+    public static void LoadScene(string name)
+    {
+        SceneToLoad = name;
+        SceneManager.LoadScene("Loading");
+    }
+
+    private void Start()
+    {
+        StartCoroutine(LoadLevelAsync());
+    }
+
+    private IEnumerator LoadLevelAsync()
+    {
+        AsyncOperation loadingOperation = SceneManager.LoadSceneAsync(SceneToLoad);
+
+        while (!loadingOperation.isDone)
+        {
+            float progress = Mathf.Clamp01(loadingOperation.progress / 0.9f);
+            if (progress < 0.1)
+                progress = 0.1f;
+            _slider.value = progress;
+            _textValueField.text = "Loading... " + (progress * 100f).ToString() + "%";
+            yield return null;
+        }
+    }
+}
